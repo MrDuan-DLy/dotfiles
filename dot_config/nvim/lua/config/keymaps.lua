66c7d569cfa46aedb04942f09024e3e7f516ba2c
Sean <duansuhang1@gmail.com>
2026-04-03 01:46:36 +0100

    Update .config/kitty/kitty.conf Update .config/nvim/lua/config/keymaps.lua Update .tmux.conf

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Alt + 方向键切换窗口（和 tmux 一致）
+vim.keymap.set("n", "<M-Left>", "<C-w>h")
+vim.keymap.set("n", "<M-Right>", "<C-w>l")
+vim.keymap.set("n", "<M-Up>", "<C-w>k")
+vim.keymap.set("n", "<M-Down>", "<C-w>j")
